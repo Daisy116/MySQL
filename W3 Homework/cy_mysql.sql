6fd3faa4172270053dd35475435eba2fab0f07ea
Daisy116 <52897966+Daisy116@users.noreply.github.com>
2019-07-31 10:03:59 +0800

    2019-07-31

---
+++ W3 Homework/cy_mysql.sql
@@ -0,0 +1,129 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2019 年 07 月 31 日 03:51
+-- 伺服器版本： 10.3.16-MariaDB
+-- PHP 版本： 7.1.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `cy_mysql`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `customer_info`
+--
+
+CREATE TABLE `customer_info` (
+  `customer_id` int(10) NOT NULL,
+  `customer_name` varchar(20) NOT NULL,
+  `headshot` blob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `product_info`
+--
+
+CREATE TABLE `product_info` (
+  `product_id` int(100) NOT NULL,
+  `product_name` varchar(30) NOT NULL,
+  `photo1` blob DEFAULT NULL,
+  `photo2` blob DEFAULT NULL,
+  `photo3` blob DEFAULT NULL,
+  `photo4` blob DEFAULT NULL,
+  `photo5` blob DEFAULT NULL,
+  `photo6` blob DEFAULT NULL,
+  `photo7` blob DEFAULT NULL,
+  `photo8` blob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `transaction_id` int(100) NOT NULL,
+  `product_id` int(100) NOT NULL,
+  `customer_id` int(100) NOT NULL,
+  `transaction_time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `customer_info`
+--
+ALTER TABLE `customer_info`
+  ADD PRIMARY KEY (`customer_id`);
+
+--
+-- 資料表索引 `product_info`
+--
+ALTER TABLE `product_info`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- 資料表索引 `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`transaction_id`),
+  ADD KEY `FK_customer_id` (`customer_id`),
+  ADD KEY `FK_product_id` (`product_id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `customer_info`
+--
+ALTER TABLE `customer_info`
+  MODIFY `customer_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `product_info`
+--
+ALTER TABLE `product_info`
+  MODIFY `product_id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `transaction_id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- 已傾印資料表的限制式
+--
+
+--
+-- 資料表的限制式 `transaction`
+--
+ALTER TABLE `transaction`
+  ADD CONSTRAINT `FK_customer_id` FOREIGN KEY (`customer_id`) REFERENCES `customer_info` (`customer_id`),
+  ADD CONSTRAINT `FK_product_id` FOREIGN KEY (`product_id`) REFERENCES `product_info` (`product_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
