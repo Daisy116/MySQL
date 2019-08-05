5c525c2fa59cb8cb3adcfc583cd498f39dc85264
Daisy116 <52897966+Daisy116@users.noreply.github.com>
2019-08-05 22:31:24 +0800

    Update data_generating.sql

--- W3 Homework/data_generating.sql
+++ W3 Homework/data_generating.sql
@@ -10,3 +10,22 @@ SELECT DATE_ADD("2017-06-15", INTERVAL @a DAY);
 試圖INSERT
 SET @a = round(RAND() * -365);
 INSERT INTO transaction (transaction_time) VALUES( DATE_ADD("2017-06-15", INTERVAL @a DAY));
+
+
+
+
+SET @a = round(RAND() * -365);
+INSERT INTO transaction (product_id,customer_id,transaction_time) VALUES(1 ,1 ,DATE_ADD("2017-06-15", INTERVAL @a DAY));
+
+
+
+
+有錯誤...
+SET @Num = 0;
+WHILE @Num <= 10  -- 當目前次數小於等於執行次數
+BEGIN
+    SET @a = round(RAND() * -365);
+    INSERT INTO transaction (product_id,customer_id,transaction_time) VALUES(1 ,1 ,DATE_ADD("2017-06-15", INTERVAL @a DAY));
+    -- 設定目前次數+1
+    SET @Num = @Num + 1
+END
